ecd4ba47c29ae709cd1a596f88d1aa51b2803aea
lawvs <18554747+lawvs@users.noreply.github.com>
2018-03-18 17:55:03 +0800

    optimization leetcode 2

--- leetcode/002.add-two-numbers.cpp
+++ leetcode/002.add-two-numbers.cpp
@@ -4,6 +4,7 @@
  *
  * 创建时间：2018-3-14 12:52:44
  * 通过时间：2018-3-14 14:14:41
+ * 优化时间：2018-3-18 17:54:07
  */
 
 /**
@@ -17,46 +18,17 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* p = (ListNode *)malloc(sizeof(*p));
-        p->next = NULL; // reference binding to misaligned address  for type 'const int', which requires 4 byte alignment
+        ListNode* p = new ListNode(0);
         ListNode* ret = p;
         int flow = 0; // 进位
-        for (; l1 && l2; l1 = l1->next, l2 = l2->next) {
-            int sum = l1->val + l2->val;
 
-            ListNode *node = (ListNode *)malloc(sizeof(*node));
-            node->next = NULL;
-            node->val = (sum + flow) % 10;
+        while (l1 || l2 || flow > 0) {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + flow;
+            p = p->next = new ListNode(sum % 10);
+            flow = sum / 10;
 
-            flow = (sum + flow) / 10;
-            p->next = node;
-            p = node;
-        }
-
-        while (l1) {
-            ListNode *node = (ListNode *)malloc(sizeof(*node));
-            node->next = NULL;
-            node->val = (l1->val + flow) % 10;
-            p->next = node;
-            flow = (l1->val + flow) / 10;
-            p = node;
-            l1 = l1->next;
-        }
-        while (l2) {
-            ListNode *node = (ListNode *)malloc(sizeof(*node));
-            node->next = NULL;
-            node->val = (l2->val + flow) % 10;
-            p->next = node;
-            flow = (l2->val + flow) / 10;
-            p = node;
-            l2 = l2->next;
-        }
-        // 进位
-        if (flow){
-            ListNode *node = (ListNode *)malloc(sizeof(*node));
-            node->next = NULL;
-            node->val = flow;
-            p->next = node;
+            l1 = l1 ? l1->next : NULL;
+            l2 = l2 ? l2->next : NULL;
         }
 
         ret = ret->next;
