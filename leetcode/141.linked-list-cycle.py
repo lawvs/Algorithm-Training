7d28df2b339d8f7610336d38f4af993d9a079871
lawvs <18554747+lawvs@users.noreply.github.com>
2018-06-07 16:31:56 +0800

    add leetcode 141
    https://leetcode.com/problems/linked-list-cycle/description/

---
+++ leetcode/141.linked-list-cycle.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+# -*- coding: utf-8 -*-
+'''
+leetcode #141 linked-list-cycle 环形链表
+https://leetcode-cn.com/problems/linked-list-cycle/description/
+'''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        p1 = p2 = head
+        while p2 and p2.next:
+            p2 = p2.next.next
+            p1 = p1.next
+            if p1 == p2:
+                return True
+        return False
