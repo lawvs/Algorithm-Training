3fd1be106890f976698c5b365c1f483caf591feb
lawvs <18554747+lawvs@users.noreply.github.com>
2018-08-19 19:47:34 +0800

    add leetcode 709

---
+++ leetcode/709.to-lower-case.go
@@ -0,0 +1,10 @@
+// leetcode #709 to-lower-case 转换成小写字母
+// https://leetcode-cn.com/problems/to-lower-case/description/
+
+// package leetcode
+
+import("strings")
+
+func toLowerCase(str string) string {
+	return strings.ToLower(str)
+}
