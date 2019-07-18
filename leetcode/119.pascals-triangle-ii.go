74b91b858e20be009254462401f849fdc7b3061c
lawvs <18554747+lawvs@users.noreply.github.com>
2019-07-19 02:49:37 +0900

    add leetcode 119

---
+++ leetcode/119.pascals-triangle-ii.go
@@ -0,0 +1,15 @@
+package main
+
+func getRow(rowIndex int) []int {
+	arr := []int{1}
+	for i := 0; i <= rowIndex; i++ {
+		t := make([]int, i+1)
+		t[0] = 1
+		t[i] = 1
+		for j := 1; j < i; j++ {
+			t[j] = arr[j] + arr[j-1]
+		}
+		arr = t
+	}
+	return arr
+}
