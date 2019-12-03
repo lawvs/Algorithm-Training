b3464d86aaeb37cd45262898783ad7af242c475e
lawvs <18554747+lawvs@users.noreply.github.com>
2019-12-04 02:30:13 +0900

    update leetcode 64
    with space optimization

--- leetcode/064.minimum-path-sum.js
+++ leetcode/064.minimum-path-sum.js
@@ -3,22 +3,18 @@
  * @return {number}
  */
 var minPathSum = function(grid) {
-  const dp = Array(grid.length)
-    .fill(0)
-    .map(() => Array(grid[0].length))
+  let dp = Array(grid[0].length)
 
-  dp[0][0] = grid[0][0]
-  for (let i = 1; i < dp.length; i++) {
-    dp[i][0] = dp[i - 1][0] + grid[i][0]
-  }
-  for (let j = 1; j < dp[0].length; j++) {
-    dp[0][j] = dp[0][j - 1] + grid[0][j]
+  dp[0] = grid[0][0]
+  for (let j = 1; j < dp.length; j++) {
+    dp[j] = dp[j - 1] + grid[0][j]
   }
 
-  for (let i = 1; i < dp.length; i++) {
-    for (let j = 1; j < dp[0].length; j++) {
-      dp[i][j] = grid[i][j] + Math.min(dp[i][j - 1], dp[i - 1][j])
+  for (let i = 1; i < grid.length; i++) {
+    dp[0] = dp[0] + grid[i][0]
+    for (let j = 1; j < dp.length; j++) {
+      dp[j] = grid[i][j] + Math.min(dp[j], dp[j - 1])
     }
   }
-  return dp[dp.length - 1][dp[0].length - 1]
+  return dp[dp.length - 1]
 }
