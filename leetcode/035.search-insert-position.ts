e7c8ecf1b817258b440f87d828933fccdd68a9d2
lawvs <18554747+lawvs@users.noreply.github.com>
2020-07-18 22:05:19 +0900

    optimization leetcode 35

--- leetcode/035.search-insert-position.ts
+++ leetcode/035.search-insert-position.ts
@@ -1,10 +1,28 @@
 function searchInsert(nums: number[], target: number): number {
-  let idx = 0
-  while (idx < nums.length) {
-    if (nums[idx] >= target) {
-      break
-    }
-    idx++
+  let start = 0
+  let end = nums.length - 1
+
+  if (nums[start] > target) {
+    return 0
   }
-  return idx
+  if (nums[end] < target) {
+    return nums.length
+  }
+
+  // Binary search
+  while (start < end) {
+    let mid = (start + end) >> 1
+    // console.log(start, end, mid)
+
+    if (nums[mid] === target) {
+      return mid
+    }
+    if (nums[mid] < target) {
+      start = mid + 1
+      continue
+    } else {
+      end = mid
+    }
+  }
+  return start
 }
