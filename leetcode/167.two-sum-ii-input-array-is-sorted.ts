8dccedd3f81c2a24312b967479028934948274e0
lawvs <18554747+lawvs@users.noreply.github.com>
2020-07-20 02:06:41 +0900

    add leetcode 167

---
+++ leetcode/167.two-sum-ii-input-array-is-sorted.ts
@@ -0,0 +1,11 @@
+function twoSum(numbers: number[], target: number): number[] {
+  let l = 0
+  let r = numbers.length - 1
+  while (l < r) {
+    const sum = numbers[l] + numbers[r]
+    if (sum === target) break
+    if (sum > target) r--
+    else l++
+  }
+  return [l + 1, r + 1]
+}
