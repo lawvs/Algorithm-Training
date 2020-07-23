6c81655083cbff6988cfa00bca7c25a366f13572
lawvs <18554747+lawvs@users.noreply.github.com>
2020-07-24 01:27:43 +0900

    add leetcode 1025

---
+++ leetcode/1025.divisor-game.go
@@ -0,0 +1,21 @@
+var m map[int]bool
+
+func divisorGame(N int) bool {
+	if m == nil {
+		m = make(map[int]bool)
+	}
+	if N == 1 {
+		return false
+	}
+	ans, ok := m[N]
+	if ok {
+		return ans
+	}
+	for i := 1; i < N; i++ {
+		if N % i == 0 && divisorGame(N - i) == false {
+			return true
+		}
+	}
+	m[N] = false
+	return false
+}
