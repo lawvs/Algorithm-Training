b537b50b95bb2ceb04d6a0a673f3b55a9eec733f
lawvs <18554747+lawvs@users.noreply.github.com>
2020-07-27 01:16:36 +0900

    add leetcode 392

---
+++ leetcode/392.is-subsequence.go
@@ -0,0 +1,15 @@
+func isSubsequence(s string, t string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	var pos int
+	for i := 0; i < len(t); i++ {
+		if s[pos] == t[i] {
+			pos++
+		}
+		if pos == len(s) {
+			return true
+		}
+	}
+	return false
+}
