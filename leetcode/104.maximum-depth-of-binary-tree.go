6a95aeaca1d99442de44ec24882c347243136a13
lawvs <18554747+lawvs@users.noreply.github.com>
2020-07-28 01:15:34 +0900

    add leetcode 104

---
+++ leetcode/104.maximum-depth-of-binary-tree.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Right == nil && root.Left == nil {
+		return 1
+	}
+	if root.Right == nil {
+		return 1 + maxDepth(root.Left) 
+	}
+	if root.Left == nil {
+		return 1 + maxDepth(root.Right) 
+	}
+	return 1 + max(maxDepth(root.Right), maxDepth(root.Left))
+}
+
+func max(x, y int) int {
+  if x > y {
+    return x
+  }
+  return y
+}
