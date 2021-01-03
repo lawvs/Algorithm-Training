7731c861da9af39e3994d85ada5ff06199b6ce27
lawvs <18554747+lawvs@users.noreply.github.com>
2021-01-04 02:53:56 +0900

    add leetcode 509

---
+++ leetcode/509.fibonacci-number.go
@@ -0,0 +1,16 @@
+var m = make(map[int]int)
+
+func fib(n int) int {
+  if m[n] != 0 {
+    return m[n]
+  }
+  if n == 0 {
+    return 0
+  }
+  if n == 1 {
+    return 1
+  }
+  ans := fib(n - 1) + fib(n - 2)
+  m[n] = ans
+  return ans
+}
