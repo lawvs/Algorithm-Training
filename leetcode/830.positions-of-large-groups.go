243b505974919fb41a48197be8068441e1a381a6
lawvs <18554747+lawvs@users.noreply.github.com>
2021-01-05 03:15:41 +0900

    add leetcode 830

---
+++ leetcode/830.positions-of-large-groups.go
@@ -0,0 +1,18 @@
+func largeGroupPositions(s string) [][]int {
+  var ans [][]int
+  var cur rune
+  start := -1
+  for pos, char := range s {
+    if char != cur {
+      if pos - start >= 3 {
+        ans = append(ans, []int{start, pos - 1})
+      }
+      cur = char
+      start = pos
+    }
+  }
+  if len(s) - start >= 3 {
+    ans = append(ans, []int{start, len(s) - 1})
+  }
+  return ans
+}
