d40441e8e54bc32c95dcd46bb7732250fb2c5900
lawvs <18554747+lawvs@users.noreply.github.com>
2021-01-08 05:25:00 +0900

    add leetcode 189

---
+++ leetcode/189.rotate-array.go
@@ -0,0 +1,25 @@
+func rotate(nums []int, k int)  {
+  l := len(nums)
+  k = k % l
+  cnt := gcd(k, l)
+  for start := 0; start < cnt; start++ {
+    cur := start
+    pre := nums[cur]
+    for {
+      // fmt.Println(nums)
+      next := (cur + k) % l
+      pre, nums[next] = nums[next], pre
+      cur = next
+      if cur == start {
+        break
+      }
+    }
+  }
+}
+
+func gcd(a, b int) int {
+  for a != 0 {
+      a, b = b % a, a
+  }
+  return b
+}
