744d257e76abd56ae23c57b86fd6d1a2d8c67275
lawvs <18554747+lawvs@users.noreply.github.com>
2021-01-11 00:45:33 +0900

    add leetcode 228

---
+++ leetcode/228.summary-ranges.go
@@ -0,0 +1,32 @@
+import (
+  "strconv"
+)
+
+func summaryRanges(nums []int) []string {
+  if len(nums) == 0 {
+    return []string{}
+  }
+  start := nums[0]
+  pre := nums[0] - 1
+  ans := []string{}
+  for _, num := range nums {
+    if pre + 1 == num {
+      pre = num
+      continue
+    }
+    if start == pre {
+      ans = append(ans, strconv.Itoa(start))
+    } else {
+      ans = append(ans, strconv.Itoa(start) + "->" + strconv.Itoa(pre))
+    }
+    start = num
+    pre = num
+  }
+
+  if start == pre {
+    ans = append(ans, strconv.Itoa(start))
+  } else {
+    ans = append(ans, strconv.Itoa(start) + "->" + strconv.Itoa(pre))
+  }
+  return ans
+}
