144c9ebd20e38424d9b86c6c41d62946fc4655c5
lawvs <18554747+lawvs@users.noreply.github.com>
2021-01-19 03:55:21 +0900

    add union find go template

---
+++ template/DisjonintSet/UnionFind.go
@@ -0,0 +1,34 @@
+
+type unionFind struct {
+	parent, rank []int
+}
+
+func newUnionFind(n int) *unionFind {
+	parent := make([]int, n)
+	rank := make([]int, n)
+	for i := range parent {
+		parent[i] = i
+		rank[i] = 1
+	}
+	return &unionFind{parent, rank}
+}
+
+func (uf *unionFind) find(x int) int {
+	if uf.parent[x] != x {
+		uf.parent[x] = uf.find(uf.parent[x])
+	}
+	return uf.parent[x]
+}
+
+func (uf *unionFind) union(x, y int) bool {
+	fx, fy := uf.find(x), uf.find(y)
+	if fx == fy {
+		return false
+	}
+	if uf.rank[fx] < uf.rank[fy] {
+		fx, fy = fy, fx
+	}
+	uf.rank[fx] += uf.rank[fy]
+	uf.parent[fy] = fx
+	return true
+}
