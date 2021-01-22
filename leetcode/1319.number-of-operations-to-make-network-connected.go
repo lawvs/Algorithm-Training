491088c53ac5cd6cb148c114377048a73c2fd9ec
lawvs <18554747+lawvs@users.noreply.github.com>
2021-01-23 03:30:40 +0900

    add leetcode 1319

---
+++ leetcode/1319.number-of-operations-to-make-network-connected.go
@@ -0,0 +1,57 @@
+package leetcode
+
+type unionFind struct {
+	parent, rank []int
+}
+
+func newUnionFind(n int) *unionFind {
+	parent := make([]int, n)
+	rank := make([]int, n)
+	for i := range parent {
+		parent[i] = i
+		rank[i] = 1
+	}
+	return &unionFind{parent, rank}
+}
+
+func (uf *unionFind) find(x int) int {
+	if uf.parent[x] != x {
+		uf.parent[x] = uf.find(uf.parent[x])
+	}
+	return uf.parent[x]
+}
+
+func (uf *unionFind) union(x, y int) bool {
+	fx, fy := uf.find(x), uf.find(y)
+	if fx == fy {
+		return false
+	}
+	if uf.rank[fx] < uf.rank[fy] {
+		fx, fy = fy, fx
+	}
+	uf.rank[fx] += uf.rank[fy]
+	uf.parent[fy] = fx
+	return true
+}
+
+func (uf *unionFind) count() int {
+	cnt := 0
+	for i := range uf.parent {
+		if uf.parent[i] == i {
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func makeConnected(n int, connections [][]int) int {
+	if len(connections) < n-1 {
+		return -1
+	}
+	uf := newUnionFind(n)
+	for _, v := range connections {
+		from, to := v[0], v[1]
+		uf.union(from, to)
+	}
+	return uf.count() - 1
+}
