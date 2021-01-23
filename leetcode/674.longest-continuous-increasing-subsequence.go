a5a463e1607b4eb57eb76f3c08cd4441f12cdd9b
lawvs <18554747+lawvs@users.noreply.github.com>
2021-01-24 03:48:19 +0900

    add leetcode 674

---
+++ leetcode/674.longest-continuous-increasing-subsequence.go
@@ -0,0 +1,25 @@
+func findLengthOfLCIS(nums []int) int {
+	l := len(nums)
+	if l == 0 {
+		return 0
+	}
+	curL := 0
+	posL := 0
+	pre := nums[0]
+	for idx, v := range nums {
+		if v <= pre {
+			curL = max(curL, idx - posL)
+			posL = idx
+		}
+		pre = v
+	}
+	curL = max(curL, l - posL)
+	return curL
+}
+
+func max(a,b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
