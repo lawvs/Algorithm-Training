cb7da5203dbc656df67afd7f2744db685c8eb503
lawvs <18554747+lawvs@users.noreply.github.com>
2021-04-25 02:08:03 +0900

    add MinHeap js template

--- template/PriorityQueue.js
+++ template/PriorityQueue.js
@@ -115,3 +115,95 @@ class PriorityQueue {
     }
   }
 }
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/kth-largest-element-in-a-stream/solution/shu-ju-liu-zhong-de-di-k-da-yuan-su-by-l-woz8/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+class MinHeap {
+  constructor(data = []) {
+    this.data = data
+    this.comparator = (a, b) => a - b
+    this.heapify()
+  }
+
+  heapify() {
+    if (this.size() < 2) return
+    for (let i = 1; i < this.size(); i++) {
+      this.bubbleUp(i)
+    }
+  }
+
+  peek() {
+    if (this.size() === 0) return null
+    return this.data[0]
+  }
+
+  push(value) {
+    this.data.push(value)
+    this.bubbleUp(this.size() - 1)
+  }
+
+  pop() {
+    if (this.size() === 0) {
+      return null
+    }
+    const result = this.data[0]
+    const last = this.data.pop()
+    if (this.size() !== 0) {
+      this.data[0] = last
+      this.bubbleDown(0)
+    }
+    return result
+  }
+
+  bubbleUp(index) {
+    while (index > 0) {
+      const parentIndex = (index - 1) >> 1
+      if (this.comparator(this.data[index], this.data[parentIndex]) < 0) {
+        this.swap(index, parentIndex)
+        index = parentIndex
+      } else {
+        break
+      }
+    }
+  }
+
+  bubbleDown(index) {
+    const lastIndex = this.size() - 1
+    while (true) {
+      const leftIndex = index * 2 + 1
+      const rightIndex = index * 2 + 2
+      let findIndex = index
+      if (
+        leftIndex <= lastIndex &&
+        this.comparator(this.data[leftIndex], this.data[findIndex]) < 0
+      ) {
+        findIndex = leftIndex
+      }
+      if (
+        rightIndex <= lastIndex &&
+        this.comparator(this.data[rightIndex], this.data[findIndex]) < 0
+      ) {
+        findIndex = rightIndex
+      }
+      if (index !== findIndex) {
+        this.swap(index, findIndex)
+        index = findIndex
+      } else {
+        break
+      }
+    }
+  }
+
+  swap(index1, index2) {
+    ;[this.data[index1], this.data[index2]] = [
+      this.data[index2],
+      this.data[index1],
+    ]
+  }
+
+  size() {
+    return this.data.length
+  }
+}
