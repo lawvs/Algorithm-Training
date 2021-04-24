73fbb5573b8cd52daf140b33c3d3e7dc1b4b7956
lawvs <18554747+lawvs@users.noreply.github.com>
2021-04-25 00:29:07 +0900

    add leetcode 377

---
+++ leetcode/377.combination-sum-iv.ts
@@ -0,0 +1,12 @@
+function combinationSum4(nums: number[], target: number): number {
+  const dp = Array(target + 1).fill(0)
+  dp[0] = 1
+  for (let i = 1; i <= target; i++) {
+    for (const num of nums) {
+      if (i - num >= 0) {
+        dp[i] += dp[i - num]
+      }
+    }
+  }
+  return dp[dp.length - 1]
+}
